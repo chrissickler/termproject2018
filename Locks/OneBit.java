7e6686168121559c0facc454c4818cd9162ae063
Mitchell <robert.whitlatch@utexas.edu>
2018-11-18 19:13:06 -0600

    OneBit made Mutually Exclusive with AtomicBooleans

--- Locks/OneBit.java
+++ Locks/OneBit.java
@@ -1,38 +1,40 @@
 package Locks;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 public class OneBit implements Lock {
 
-    private volatile boolean[] b;
+    private AtomicBoolean[] b;
     private int numThreads;
 
     public OneBit(int numThreads) {
         this.numThreads = numThreads;
-        b = new boolean[numThreads+1];
+        b = new AtomicBoolean[numThreads+1];
         for(int i = 0; i < numThreads+1; i++) {
-            b[i] = false;
+            b[i] = new AtomicBoolean(false);
         }
     }
 
     public void lock(int threadID) {
         int j;
         do {
-            b[threadID] = true;
+            b[threadID].set(true);
             j = 1;
-            while((b[threadID] == true) && (j < threadID)) {
-                if(b[j] == true) {
-                    b[threadID] = false;
-                    while(b[j] == true);
+            while((b[threadID].get() == true) && (j < threadID)) {
+                if(b[j].get() == true) {
+                    b[threadID].set(false);
+                    while(b[j].get() == true);
                 }
                 j++;
             }
-        } while(b[threadID] != true);
+        } while(b[threadID].get() != true);
         for(j = threadID + 1; j < numThreads+1; j++) {
-            while(b[j] == true);
+            while(b[j].get() == true);
         }
     }
 
     public void unlock(int threadID) {
-        b[threadID] = false;
+        b[threadID].set(false);
     }
 
 }
